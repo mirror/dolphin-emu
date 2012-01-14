34f9dfaeeab73832af749a6f494123737f148954
Ryan Houdek <Sonicadvance1@gmail.com>
2012-01-14 01:20:41 -0600

    Need to move DLCache JIT code to a seperate file, Currently compiling in X86ABI on generic host or else many problems arise. Need to compile in with all the x86 emitter functions as well or more problems arise for now. Currently only emitter that might be correct for ARM is NOP :D

--- Source/Plugins/Plugin_VideoOGL/Src/Render.cpp
+++ Source/Plugins/Plugin_VideoOGL/Src/Render.cpp
@@ -1396,7 +1396,9 @@ void Renderer::Swap(u32 xfbAddr, FieldType field, u32 fbWidth, u32 fbHeight,cons
 	GL_REPORT_ERRORD();
 
 	// Clean out old stuff from caches. It's not worth it to clean out the shader caches.
+#ifndef _M_GENERIC
 	DLCache::ProgressiveCleanup();
+#endif
 	TextureCache::Cleanup();
 
 	frameCount++;
