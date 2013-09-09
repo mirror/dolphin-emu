513bf1be2fa97e103de70e24f3b84c5233bb7435
comex <comexk@gmail.com>
2013-09-08 23:57:49 -0400

    Complain about threadsafe events during netplay.

--- Source/Core/Core/Src/CoreTiming.cpp
+++ Source/Core/Core/Src/CoreTiming.cpp
@@ -15,6 +15,10 @@
 
 #define MAX_SLICE_LENGTH 20000
 
+namespace NetPlay {
+	bool IsNetPlayRunning();
+};
+
 namespace CoreTiming
 {
 
@@ -22,6 +26,7 @@ struct EventType
 {
 	TimedCallback callback;
 	const char *name;
+	bool did_whine;
 };
 
 std::vector<EventType> event_types;
@@ -200,6 +205,17 @@ u64 GetIdleTicks()
 // schedule things to be executed on the main thread.
 void ScheduleEvent_Threadsafe(int cyclesIntoFuture, int event_type, u64 userdata)
 {
+	if (NetPlay::IsNetPlayRunning() && !Core::IsCPUThread())
+	{
+		EventType& et = event_types[event_type];
+		if (!et.did_whine)
+		{
+			WARN_LOG(POWERPC, "Someone scheduled an off-thread \"%s\" event while netplay was active.  "
+			                  "This is likely to cause a desync.", et.name);
+			// don't care about thread safety
+			et.did_whine = true;
+		}
+	}
 	std::lock_guard<std::mutex> lk(tsWriteLock);
 	Event ne;
 	ne.time = globalTimer + cyclesIntoFuture;
