9d94db30f13a1e10789ecd76a85654b8c0a4fa47
Shawn Hoffman <godisgovernment@gmail.com>
2013-09-24 01:50:17 -0700

    [vc12] let dx11 compile shaders

--- Source/Plugins/Plugin_VideoDX11/Src/D3DShader.cpp
+++ Source/Plugins/Plugin_VideoDX11/Src/D3DShader.cpp
@@ -4,6 +4,9 @@
 
 #include <string>
 
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
+
 #include "VideoConfig.h"
 
 #include "D3DBase.h"
@@ -37,9 +40,8 @@ bool CompileVertexShader(const char* code, unsigned int len, D3DBlob** blob)
 #else
 	UINT flags = D3D10_SHADER_ENABLE_BACKWARDS_COMPATIBILITY|D3D10_SHADER_OPTIMIZATION_LEVEL3|D3D10_SHADER_SKIP_VALIDATION;
 #endif
-	//HRESULT hr = PD3DX11CompileFromMemory(code, len, NULL, NULL, NULL, "main", D3D::VertexShaderVersionString(),
-	//						flags, 0, NULL, &shaderBuffer, &errorBuffer, NULL);
-	HRESULT hr = 0;
+	HRESULT hr = D3DCompile(code, len, NULL, NULL, NULL, "main", D3D::VertexShaderVersionString(),
+							flags, 0, &shaderBuffer, &errorBuffer);
 	
 	if (errorBuffer)
 	{
@@ -96,9 +98,8 @@ bool CompileGeometryShader(const char* code, unsigned int len, D3DBlob** blob,
 #else
 	UINT flags = D3D10_SHADER_ENABLE_BACKWARDS_COMPATIBILITY|D3D10_SHADER_OPTIMIZATION_LEVEL3|D3D10_SHADER_SKIP_VALIDATION;
 #endif
-	//HRESULT hr = PD3DX11CompileFromMemory(code, len, NULL, pDefines, NULL, "main", D3D::GeometryShaderVersionString(),
-	//						flags, 0, NULL, &shaderBuffer, &errorBuffer, NULL);
-	HRESULT hr = 0;
+	HRESULT hr = D3DCompile(code, len, NULL, pDefines, NULL, "main", D3D::GeometryShaderVersionString(),
+							flags, 0, &shaderBuffer, &errorBuffer);
 
 	if (errorBuffer)
 	{
@@ -157,9 +158,8 @@ bool CompilePixelShader(const char* code, unsigned int len, D3DBlob** blob,
 #else
 	UINT flags = D3D10_SHADER_OPTIMIZATION_LEVEL3;
 #endif
-	//HRESULT hr = PD3DX11CompileFromMemory(code, len, NULL, pDefines, NULL, "main", D3D::PixelShaderVersionString(),
-	//						flags, 0, NULL, &shaderBuffer, &errorBuffer, NULL);
-	HRESULT hr = 0;
+	HRESULT hr = D3DCompile(code, len, NULL, pDefines, NULL, "main", D3D::PixelShaderVersionString(),
+							flags, 0, &shaderBuffer, &errorBuffer);
 
 	if (errorBuffer)
 	{
