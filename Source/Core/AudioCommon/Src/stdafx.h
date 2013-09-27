75fec84c16cb6fb402b9b16ca05c008f85819856
Shawn Hoffman <godisgovernment@gmail.com>
2013-09-26 23:41:17 -0700

    [vc12] remove dx9 vcxproj from sln. Allow XAudio2 stuff to build for older environments when June 2010 DirectX SDK is installed. It remains to be seen if it's runnable across all environments, however.

--- Source/Core/AudioCommon/Src/stdafx.h
+++ Source/Core/AudioCommon/Src/stdafx.h
@@ -4,7 +4,12 @@
 
 #pragma once
 
+#ifdef HAVE_DXSDK_JUNE_2010
+#define _WIN32_WINNT 0x501
+#else
+#pragma message("Resulting binary will be compatible with DirectX >= Windows 8. Install the June 2010 DirectX SDK if you want to build for older environments.")
 #define _WIN32_WINNT 0x602
+#endif
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
