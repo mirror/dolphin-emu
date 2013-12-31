bf2ddd4eebf0b8b6483061e734c093b5f11a40a2
Ryan Houdek <Sonicadvance1@gmail.com>
2013-12-30 19:45:24 -0600

    [GL-AutoChoose] Remove a duplicated GLInterfaceMode enum. Allow GLInterface(EGL) to auto detect which OpenGL level we are running at. It will iterate through the EGLConfigs and check for GL/GLES3/GLES2 support, and auto create a context with the highest level of support. Everything but EGL runs in only GL mode as usual.

--- Source/Core/VideoBackends/OGL/Src/main.cpp
+++ Source/Core/VideoBackends/OGL/Src/main.cpp
@@ -179,10 +179,7 @@ bool VideoBackend::Initialize(void *&window_handle)
 	UpdateActiveConfig();
 
 	InitInterface();
-	GLInterface->SetMode(GLInterfaceMode::MODE_OPENGL);
-#ifdef USE_GLES3
-	GLInterface->SetMode(GLInterfaceMode::MODE_OPENGLES3);
-#endif
+	GLInterface->SetMode(GLInterfaceMode::MODE_DETECT);
 	if (!GLInterface->Create(window_handle))
 		return false;
 
